f1a6db400e830d565a23a7dc426194efa29d4cfe
[omitted] <>
2021-04-28 05:58:24 +0900

    update

--- testToTestTool/test1.test.js
+++ testToTestTool/test1.test.js
@@ -1,13 +1,15 @@
-describe('testGroupName', () => {
+group('testGroupName', () => {
   test('testName', function() {
     const received = 100
     const expected = 100
-    expect(received).toBe(expected)
+
+    check(received, expected)
   })
 
   test('Fail test', function() {
     const received = 100
     const expected = 20
-    expect(received).toBe(expected)
+
+    check(received, expected)
   })
 })
