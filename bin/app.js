9e28709d45e23fad71db2d5ab2c3a85133678e18
[omitted] <>
2021-04-27 12:57:15 +0900

    change variable name.

--- bin/app.js
+++ bin/app.js
@@ -4,8 +4,8 @@ const Unitest = require('../lib/unitest')
 const asyncFs = require('../lib/asyncFs')
 
 const main = async () => {
-  const configString = await asyncFs.readFile('./.unitest.json', 'utf8')
-  const config = JSON.parse(configString)
+  const configText = await asyncFs.readFile('./.unitest.json', 'utf8')
+  const config = JSON.parse(configText)
   const unitest = new Unitest(config)
   unitest.run('./')
 }
